3e32649aaf86948ac845f1049fa8733be904b008
ampersand-paris <ampersand.s.paris@gmail.com>
2023-08-02 16:00:12 -0400

    footer styled - missing mailchimp integration

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,14 +2,13 @@ import { BrowserRouter as Router,
   Routes,
   Route } from "react-router-dom";
 import Navigation from "./partials/Navigation"
+import Footer from "./partials/Footer";
 
 function App() {
   return (
     <Router>
       <Navigation />
-      <Routes>
-        
-      </Routes>
+      <Footer />
     </Router>
   );
 }
