23d9098ae10f7357acfe24ee0ea4b0de20443061
ampersand-paris <ampersand.s.paris@gmail.com>
2024-06-01 12:35:29 -0400

    archive page update

--- frontend/src/pages/ArchiveDetail.js
+++ frontend/src/pages/ArchiveDetail.js
@@ -58,7 +58,7 @@ const ArchiveDetail = (props) => {
                 </div>
                 <div className="description-performances-container">
                     <div className="project-description">
-                        <p>{project.Description}</p>
+                        <ReactMarkdown>{project.Description_Rich_Text}</ReactMarkdown>
                         <ReactMarkdown className="press-quote">{project.Press_Quote}</ReactMarkdown>
                     </div>
                     <div className="performances">
@@ -86,11 +86,12 @@ const ArchiveDetail = (props) => {
                     }
                     <div className="credits-header">
                         <h3>Funders</h3>
+                        <ReactMarkdown>{project.Funders}</ReactMarkdown>
                     </div>
                 </div>
                 <div className="description-performances-container">
                     <div className="project-description">
-                        <ReactMarkdown>{project.Funders}</ReactMarkdown>
+                        <ReactMarkdown>{project.Bebe_Writing}</ReactMarkdown>
                     </div>
                     <div className="performances">
                    
